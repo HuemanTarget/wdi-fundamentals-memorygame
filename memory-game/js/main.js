ca4335ebc5503c8d9ff98d875cd7e74108c21152
HuemanTarget <38532350+HuemanTarget@users.noreply.github.com>
2019-11-02 12:05:20 -0700

    Add files via upload
    added game logic

---
+++ memory-game/js/main.js
@@ -0,0 +1,9 @@
+console.log("Up and running!");
+
+var cardOne = "queen diamonds";
+var cardTwo = "king diamonds";
+var cardThree = "queen hearts";
+var cardFour = "king hearts";
+
+console.log("User flipped" + cardOne);
+console.log("User flipped" + cardThree);
